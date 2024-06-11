5756c3ede26c563b8fdcc8ff7c7069582526c666
Hydraallen <wangruiallen@gmail.com>
2024-06-11 23:50:32 +0800

    add: search script

---
+++ search_word.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 定义要搜索的目录
+SEARCH_DIR="./"
+# 定义要搜索的短语
+SEARCH_PHRASE="Searching..."
+
+# 使用find和grep搜索包含短语的文件
+echo "Searching word '${SEARCH_PHRASE}' in file..."
+find "$SEARCH_DIR" -type f -exec grep -l "$SEARCH_PHRASE" {} +
+
+echo "Done."
+
